f63ab5f6d1be3f8c083e685df9eb2887ca8633f8
Code Sandbox <hello@codesandbox.io>
2022-01-23 13:44:13 +0000

    Deployment from Codesandbox.

---
+++ index.js
@@ -0,0 +1,19 @@
+function morshuFunc() {
+  window.open("https://youtu.be/PxlyQJ1umWc");
+}
+function romanusFunc() {
+  window.open("https://youtu.be/s2p51Of_QJ8");
+}
+function mongoFunc() {
+  window.open("https://youtu.be/p_5yt5IX38I");
+}
+function thanosFunc() {
+  window.open("https://youtu.be/6WQpVPZo70Y");
+}
+
+function MetehanFunc() {
+  window.open("metehan.html");
+}
+function meteganFunc() {
+  window.open("https://youtu.be/2TsI8bpB9Yo");
+}
